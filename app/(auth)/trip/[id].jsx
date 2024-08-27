1669e269776aa31aabe6848efd16136fd7a36a2e
Villarejo Maxime <maxime.villarejo@he-arc.ch>
2024-08-27 17:33:56 +0200

    add header and manage trip menu

--- app/(auth)/trip/[id].jsx
+++ app/(auth)/trip/[id].jsx
@@ -1,28 +1,223 @@
 import { useLocalSearchParams } from 'expo-router';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Dimensions, SafeAreaView, TouchableOpacity } from 'react-native';
+import { MenuProvider, Menu, MenuTrigger, MenuOptions, MenuOption } from 'react-native-popup-menu';
 import { useRouter } from 'expo-router';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { db, auth } from '../../../firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import COLORS from '../../../styles/COLORS';
+
 import ShareTripModal from '../../../components/ShareTripModal';
+import { Feather, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
 export default function DetailsScreen() {
   const { id } = useLocalSearchParams();
   const [openShare, setOpenShare] = useState(false);
   const router = useRouter();
+  const [title, setTitle] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [comment, setComment] = useState('');
+  const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const [canEdit, setCanEdit] = useState(false);
+  const [canShare, setCanShare] = useState(false);
+  const [canDelete, setCanDelete] = useState(false);
+
+  useEffect(() => {
+    const fetchTripDetails = async () => {
+      try {
+        const docRef = doc(db, 'trips', id); // 'trips' est le nom de votre collection
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const tripData = docSnap.data();
+          setTitle(tripData.title);
+
+          setStartDate(tripData.startDate.toDate().toLocaleDateString());
+          setEndDate(tripData.endDate.toDate().toLocaleDateString());
+
+          setComment(tripData.comment);
+          setImage(tripData.image);
+
+          // Set permissions based on user's role
+          const userId = auth.currentUser.uid;
+          if (userId === tripData.uid) {
+            setCanEdit(true);
+            setCanShare(true);
+            setCanDelete(true);
+          } else if (tripData.canWrite.includes(userId)) {
+            setCanEdit(true);
+          }
+        } else {
+          console.log('No such document!');
+        }
+      } catch (error) {
+        console.error('Error fetching trip details: ', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (id) {
+      fetchTripDetails();
+    }
+  }, [id]);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={{ color: "white" }}>Details of user {id}</Text>
-      <Button title="Go back" onPress={() => router.back()} />
-      <Button title="Open Share Modal" onPress={() => setOpenShare(true)} />
-      <ShareTripModal isOpen={openShare} onCancel={()=>setOpenShare(!openShare)} onConfirm={()=>setOpenShare(!openShare)} tripCode={id}/>
-    </View>
+    <MenuProvider>
+      <View style={styles.container}>
+        <ImageBackground source={{ uri: image }} style={styles.imageBackground}>
+          <View style={styles.imageOverlay} />
+          <SafeAreaView style={styles.overlay}>
+            <View style={styles.header}>
+              <TouchableOpacity onPress={() => router.back()}>
+                <Feather name="arrow-left" size={30} color="white" />
+              </TouchableOpacity>
+              <Text style={styles.imageTitle}>{title}</Text>
+              <Menu>
+                <MenuTrigger>
+                  <MaterialCommunityIcons name="dots-horizontal" size={30} color="white" />
+                </MenuTrigger>
+                <MenuOptions style={styles.menuOptions}>
+                    <MenuOption 
+                      style={[styles.menuItems, { opacity: canShare ? 1 : 0.5}]}
+                      onSelect={canShare ? () => setOpenShare(true) : null} 
+                      disabled={!canShare}
+                    >
+                      <Text style={styles.menuOptionText}>SHARE</Text>
+                      <MaterialIcons style={{ padding: 10 }} name="share" size={20} color="white" />
+                    </MenuOption>
+                    <View style={styles.menuDivider} />
+                    <MenuOption 
+                      style={[styles.menuItems, { opacity: canEdit ? 1 : 0.5}]}
+                      onSelect={canEdit ? () => console.log('Edit') : null} 
+                      disabled={!canEdit}
+                    >
+                      <Text style={styles.menuOptionText}>EDIT</Text>
+                      <MaterialIcons style={{ padding: 10 }} name="edit" size={20} color="white" />
+                    </MenuOption>
+                    <View style={styles.menuDivider} />
+                    <MenuOption 
+                      style={styles.menuItems} 
+                      onSelect={canDelete ? () => console.log('Delete') : console.log('Quit')} 
+                    >
+                      <Text style={styles.menuOptionText}>{canDelete ? 'DELETE' : 'QUITTER'}</Text>
+                      <MaterialIcons style={{ padding: 10 }} name="delete" size={20} color="white" />
+                    </MenuOption>
+                </MenuOptions>
+              </Menu>
+              <ShareTripModal isOpen={openShare} onCancel={() => setOpenShare(false)} onConfirm={() => setOpenShare(false)} tripCode={id} />
+            </View>
+            <View>
+              <Text style={styles.imageComment}>{comment}</Text>
+            </View>
+            <View style={styles.imageDateContainer}>
+              <MaterialCommunityIcons style={[{ transform: [{ rotate: '20deg' }] }, styles.imageDates]} name="airplane" size={20} color="white" />
+              <Text style={styles.imageDates}> {startDate}</Text>
+              <MaterialCommunityIcons style={[{ transform: [{ rotate: '70deg' }], marginLeft: 25 }, styles.imageDates]} name="airplane" size={20} color="white" />
+              <Text style={styles.imageDates}> {endDate}</Text>
+            </View>
+          </SafeAreaView>
+        </ImageBackground>
+      </View>
+    </MenuProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  imageBackground: {
+    width: '100%',
+    height: Dimensions.get('window').height / 3,
+    alignItems: 'center',
+    shadowColor: COLORS.dark,
+    shadowOffset: {
+      height: 2,
+    },
+    shadowOpacity: 1,
+    elevation: 5,
+  },
+  imageOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    justifyContent: 'space-between',
+    marginVertical: 20,
+    width: '90%',
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  imageTitle: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+
+  imageComment: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+
+  imageDateContainer: {
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  imageDates: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+
+  menuOptions: {
+    backgroundColor: COLORS.background_dark,
+    padding: 10,
+    borderColor: COLORS.light_grey,
+    borderWidth: 1,
+  },
+
+  menuOptionText: {
+    color: 'white',
+    fontSize: 16,
+    padding: 10,
+  },
+  
+  menuDivider: {
+    height: 1,
+    backgroundColor: COLORS.light_grey,
+    marginVertical: 5,
+    width: '85%',
+    alignSelf: 'center',
+  }, 
+
+  menuItems: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
 });
